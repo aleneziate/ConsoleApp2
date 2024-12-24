328bdc4de356165ccb10bfeefd557dfd998cae88
alene <alene@ABDALLAH>
2024-12-24 14:47:50 +0300

    Add project files.

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using ConsoleApp2.student;
+
+Students students = new Students();
+string name, GPA, major;
+Console.WriteLine("Enter Name ");
+name = Console.ReadLine();
+Console.WriteLine("Enter Major");
+major = Console.ReadLine();
+Console.WriteLine("Enter your GPA");
+GPA = Console.ReadLine();
+Console.WriteLine(students.GetInfo(name, major, GPA));
+
+
+Console.WriteLine("------------------");
+
+//Task 2
+
---
+++ ConsoleApp2/student/Students.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace ConsoleApp2.student
+{
+    public class Students
+    {
+        public string Name;
+        public string Major;
+        public string GPA;
+
+        public string GetInfo(string Name, string Major, string GPA)
+        {
+            return Name + " " + Major + " " + GPA;
+            
+        }
+    
+      }
+}
